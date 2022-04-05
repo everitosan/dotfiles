19bebb6191d8ae0bc37c1ec60d3ae8b904ca1b50
evesan <everardo.ipn@gmail.com>
2022-04-05 13:54:11 -0500

    refact: dont upload config files as will be tracked by github

--- calcurse/config/scripts/sync.sh
+++ calcurse/config/scripts/sync.sh
@@ -7,15 +7,15 @@ CONFIG_DIR="${HOME}/.config/calcurse"
 function up {
   # Uploads calendar data
   aws s3 sync "${DATA_DIR}" "${BUCKET}/data" --exclude "*.pid"
-  # Uploads config calendar data
-  aws s3 sync "${CONFIG_DIR}" "${BUCKET}/config" --exclude "*.pid" 
+  # Uploads config calendar 
+  # aws s3 sync "${CONFIG_DIR}" "${BUCKET}/config" --exclude "*.pid" 
 }
 
 function down {
   # Downloads calendar data
   aws s3 sync "${BUCKET}/data" "${DATA_DIR}" --exclude "*.pid" 
-  # Donwloads config calendar data
-  aws s3 sync "${BUCKET}/config" "${CONFIG_DIR}" --exclude "*.pid" 
+  # Donwloads config calendar 
+  # aws s3 sync "${BUCKET}/config" "${CONFIG_DIR}" --exclude "*.pid" 
 }
 
 while getopts "ud" OPTS; do
